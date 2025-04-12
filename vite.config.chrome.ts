7642887b571f8ade879d5fd68a29275fbe114551
Felipe Rodrigues <me@felipefr.dev>
2025-04-12 16:35:37 -0300

    feat: create browser configuration for Chrome and Firefox

--- vite.config.chrome.ts
+++ vite.config.chrome.ts
@@ -1,30 +1,3 @@
-import { resolve } from "path";
-import { mergeConfig, defineConfig } from "vite";
-import { crx, ManifestV3Export } from "@crxjs/vite-plugin";
-import baseConfig, { baseManifest } from "./vite.config.base";
+import { createBrowserConfig } from './vite.config.browser'
 
-const outDir = resolve(__dirname, "dist_chrome");
-
-export default mergeConfig(
-  baseConfig,
-  defineConfig({
-    plugins: [
-      crx({
-        manifest: {
-          ...baseManifest,
-          background: {
-            service_worker: "src/background.ts",
-            type: "module",
-          },
-        } as ManifestV3Export,
-        browser: "chrome",
-        contentScripts: {
-          injectCss: true,
-        },
-      }),
-    ],
-    build: {
-      outDir,
-    },
-  })
-);
+export default createBrowserConfig({ browser: 'chrome', outDir: 'dist_chrome' })
