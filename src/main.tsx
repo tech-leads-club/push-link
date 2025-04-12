eee12e6f7a17100faefea14dba9b5c43d5ce757b
Felipe Rodrigues <me@felipefr.dev>
2025-04-12 16:35:50 -0300

    refactor: simplify root element rendering in main component

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { Popup } from "./extension/popup/index";
-import "./extension/global.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './extension/global.css'
+import { Popup } from './extension/popup/index'
 
-createRoot(document.getElementById("root")!).render(
+const domNode = document.getElementById('root')
+const root = createRoot(domNode as HTMLElement)
+
+root.render(
   <StrictMode>
     <div className="bg-stone-900 w-[500px] h-[700px]">
       <Popup />
     </div>
-  </StrictMode>
-);
+  </StrictMode>,
+)
