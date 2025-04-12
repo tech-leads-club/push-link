b0aba8f6d7d4c052d754fd92fa8f7888c6f74c5f
Felipe Rodrigues <me@felipefr.dev>
2025-04-12 16:35:42 -0300

    refactor: update import statements and streamline configuration

--- vite.config.base.ts
+++ vite.config.base.ts
@@ -1,42 +1,39 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { viteStaticCopy } from "vite-plugin-static-copy";
-import { ManifestV3Export } from "@crxjs/vite-plugin";
-import { resolve } from "path";
-import manifest from "./manifest.json";
-import pkg from "./package.json";
+import { ManifestV3Export } from '@crxjs/vite-plugin'
+import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import manifest from './manifest.json'
+import pkg from './package.json'
 
 export const baseManifest = {
   ...manifest,
-  version: pkg.version,    
-} as ManifestV3Export;
+  version: pkg.version,
+} as ManifestV3Export
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    tsconfigPaths(),
     viteStaticCopy({
       targets: [
-        { src: "./public/16.png", dest: "./public" },
-        { src: "./public/32.png", dest: "./public" },
-        { src: "./public/48.png", dest: "./public" },
-        { src: "./public/192.png", dest: "./public" },
-        { src: "./public/logo.png", dest: "./public" },
+        { src: './public/16.png', dest: './public' },
+        { src: './public/32.png', dest: './public' },
+        { src: './public/48.png', dest: './public' },
+        { src: './public/192.png', dest: './public' },
+        { src: './public/logo.png', dest: './public' },
       ],
     }),
   ],
-  publicDir: resolve(__dirname, "public"),
+  publicDir: resolve(__dirname, 'public'),
   build: {
     rollupOptions: {
       input: {
-        popup: resolve(__dirname, "popup.html"),
-        options: resolve(__dirname, "options.html"),
-        background: resolve(__dirname, "src/background.ts"),
-        content: resolve(__dirname, "src/content.ts"),
+        popup: resolve(__dirname, 'popup.html'),
+        options: resolve(__dirname, 'options.html'),
       },
-      output: {
-        entryFileNames: "[name].js",
-      },
+      output: { entryFileNames: '[name].js' },
     },
   },
-});
+})
