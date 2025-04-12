fbdf8ba9909a013a9a7e926dae302954919223d2
Felipe Rodrigues <me@felipefr.dev>
2025-04-12 16:30:51 -0300

    refactor: merge base configuration into Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { viteStaticCopy } from "vite-plugin-static-copy";
-import { resolve } from "path";
+import { defineConfig, mergeConfig } from 'vite'
+import baseConfig from './vite.config.base'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        { src: "src/extension/manifest.json", dest: "." },
-        { src: "src/extension/public/16.png", dest: "./public" },
-        { src: "src/extension/public/32.png", dest: "./public" },
-        { src: "src/extension/public/48.png", dest: "./public" },
-        { src: "src/extension/public/192.png", dest: "./public" },
-        { src: "src/extension/public/logo.png", dest: "./public" },
-      ],
-    }),
-  ],
-  build: {
-    rollupOptions: {
-      input: {
-        popup: resolve(__dirname, "popup.html"),
-        options: resolve(__dirname, "options.html"),
-        background: resolve(__dirname, "src/background.ts"),
-        content: resolve(__dirname, "src/content.ts"),
-      },
-      output: {
-        entryFileNames: "[name].js",
-      },
-    },
-  },
-});
+export default mergeConfig(
+  baseConfig,
+  defineConfig({
+    // Add dev environment configuration if needed
+  }),
+)
