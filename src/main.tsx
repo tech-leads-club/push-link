d7e11e2efefe4cdd2a0017c631f021c2aa53da1a
William Reis Fernandes <william@mapall.com>
2025-04-15 13:00:21 +0100

    chore: add posthog and basic integration

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,24 @@
+import posthog from 'posthog-js'
+import { PostHogProvider } from 'posthog-js/react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
 import './extension/global.css'
 import { Popup } from './extension/popup/index'
+
+posthog.init(import.meta.env.VITE_PUBLIC_POSTHOG_KEY, {
+  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+})
 
 const domNode = document.getElementById('root')
 const root = createRoot(domNode as HTMLElement)
 
 root.render(
   <StrictMode>
-    <div className="bg-gray-900 w-[320px] h-[480px]">
-      <Popup />
-    </div>
+    <PostHogProvider client={posthog}>
+      <div className="bg-gray-900 w-[320px] h-[480px]">
+        <Popup />
+      </div>
+    </PostHogProvider>
   </StrictMode>,
 )
