b7b78d5e348348f2640cc7da99598c4c14f25c2b
Felipe <felipe@example.com>
2025-08-11 13:28:36 -0300

    feat: add extension URLs for Firefox and Chrome

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -10,3 +10,9 @@ export const REQUIRED_COOKIES: CookieInfo[] = [
   { name: 'user_session_identifier' },
   { name: '_circle_session' },
 ]
+
+export const EXTENSION_URLS = {
+  firefox: 'https://addons.mozilla.org/en-US/firefox/addon/tech-leads-club-push-link/',
+  chrome:
+    'https://chromewebstore.google.com/detail/techleadsclub-push-link/opdkcagikdagcabfckgidpkmcibphjlc?authuser=1&hl=en',
+}
